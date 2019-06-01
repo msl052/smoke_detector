1e4c0e119c128cff8db6877df16cbc2d4eaaa46b
Min <msl052@ucsd.edu>
2019-06-01 16:41:15 -0700

    julie's code to push to heroku

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var	  express		  = require('express'),
+/*var	  express		  = require('express'),
 	  bodyParser 	  = require('body-parser'),
 	  app	      	  = express(),
 	  twilio        = require('twilio');
@@ -9,19 +9,19 @@ var 	indexRoutes 	 = require('./routes/index'),
 app.set('port', (process.env.PORT || 3000));
 /*app.get('/', (req,res) => {
   res.send("I am loading the home page");
-});*/
+});*//*
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/assets'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+/*
 
 /*************** ROUTES ****************/
-
+/*
 app.use('/', indexRoutes);
 app.use('/api', apiRoutes);
-
+*/
 /*
 app.get('/', (req,res) => {
         res.render('user');
@@ -53,7 +53,7 @@ app.post('/configure',  (req,res) =>{
 
 });
 */
-
+/*
 app.listen(app.get('port'),() => console.log('Listening on port ' + app.get('port')));
 //app.get('/api/sms');
 
@@ -65,6 +65,7 @@ client.messages
   .create({
     body: 'whats up?',
     from: "+14245328392",
+    statusCallback: '/api/sms',
     to  : "+18186352564"
    })
   .then(message => console.log(message.sid));
@@ -74,6 +75,97 @@ client.messages
 const http = require('http');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
+
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+  console.log('Hello');
+  
+  twiml.message('The Robots are coming! Head for the hills!');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(1337, () => {
+  console.log('Express server listening on port 1234');
+});
+
+*/
+var	  express		  = require('express'),
+  	  bodyParser 	= require('body-parser'),
+  	  twilio      = require('twilio'),
+      app         =express(),
+      http        =require('http');
+
+
+
+var
+	    indexRoutes 	 = require('./routes/index'),
+      apiRoutes        = require('./routes/api');
+
+     
+
+
+app.set('port', (process.env.PORT || 3000));
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/assets'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+/*************** ROUTES ****************/
+
+app.use('/', indexRoutes);
+app.use('/api', apiRoutes);
+
+
+/**************Text******************/
+
+const account_sid = "ACb63c109c52daf392846e20ed706d4afd"
+const auth_token = "99e800ac8597937082f6e0e946d66fa7"
+const client = require('twilio')(account_sid,auth_token);
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+client.messages
+  .create({
+     body: 'julies code?',
+      from: "+14245328392",
+     to: "+18186352564"
+   })
+  .then(message => console.log(message.sid));
+
+
+/****************Response********************/
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  if (req.body.Body == 'Yes') {
+    twiml.message('Help is on the way');
+  } else if (req.body.Body == 'No') {
+    twiml.message('Glad to hear. Have a good day!');
+  } else {
+    twiml.message(
+      'Please respond Yes or No'
+    );
+  }
+
+  res.writeHead(200, { 'Content-Type': 'text/xml' });
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(1337, () => {
+  console.log('Express server listening on port 1337');
+});
+
+
+  /*
+
+app.listen(app.get('port'),() => console.log('Listening on port ' + app.get('port')));
 
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
@@ -87,3 +179,5 @@ app.post('/sms', (req, res) => {
 http.createServer(app).listen(1337, () => {
   console.log('Express server listening on port 3000');
 });
+
+*/
