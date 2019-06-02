5ee14dc33d5dee67836ac181dea45fb2b283f451
Hickery <msl052@ucsd.edu>
2019-06-01 22:22:32 -0700

    4 testing response text

--- helpers/sms.js
+++ helpers/sms.js
@@ -1,5 +1,6 @@
+import myVar from './app.js'
 var db = require('../models');
-var ap = require('../')
+//var ap = require('../')
 
 exports.sendSMS = function(req, res) {
     // Download the helper library from https://www.twilio.com/docs/node/install
@@ -30,10 +31,10 @@ exports.callBack = function(req, res) {
 
     if (req.body.Body == 'Yes') {
         twiml.message('Help is on the way');
-        clearTimeout(ap.myVar);
+        clearTimeout(myVar);
     } else if (req.body.Body == 'No') {
         twiml.message('Glad to hear. Have a good day!');
-        clearTimeout(ap.myVar);
+        clearTimeout(myVar);
     } else {
         twiml.message(
           'Please respond Yes or No'
