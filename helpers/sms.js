c6a89c18c3a7d3b234be7529a47ce265152cdf8c
Min <msl052@ucsd.edu>
2019-06-03 13:22:46 -0700

    36 texting response test

--- helpers/sms.js
+++ helpers/sms.js
@@ -15,7 +15,13 @@ var name = "Min Suk Lee";
 
 var myVar;
 
-emergencyNumber = db.userInfo.emergencyNumber
+ db.userInfo.findOne({})
+    .then( function(result) {
+      //console.log(result.phoneNumber);
+      phoneNumber = result.phoneNumber;
+      emergencyNumber = result.emergencyNumber;
+      //console.log(result.phoneNumber);
+    });
 
 exports.sendSMS = function(req, res) {
     // Download the helper library from https://www.twilio.com/docs/node/install
